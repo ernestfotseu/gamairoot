a0c6f7d4fe8098dfcf5f01b000e9f203f5460c68
$uname <$email>
2022-11-26 17:21:19 +0000

    User ernestfotseu : 1st commit

--- pushgamaiongithub.sh
+++ pushgamaiongithub.sh
@@ -13,13 +13,13 @@ git config --global user.name "${usernamespecify}"
 git init
 git add .
 echo "*****Commit description : ${descriptioncommit}*****"
-git commit -m 'User ${usernamespecify} : ${descriptioncommit}'
+git commit -m "User ${usernamespecify} : ${descriptioncommit}"
 echo "*****Remove old origin*****"
 git remote remove origin
 echo "*****Add origin*****"
 # git remote add origin https://github.com/ernestfotseu/gamairoot-ce.git
 git remote add origin https://${acces_token}@github.com/ernestfotseu/gamairoot-ce.git
-echo "https://${acces_token}@github.com/ernestfotseu/gamairoot-ce.git"
+echo "https://$acces_token@github.com/ernestfotseu/gamairoot-ce.git"
 echo "*****Pull github origin*****"
 git pull origin main --rebase
 git remote -v
