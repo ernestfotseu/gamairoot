6616ecb17e113657b64c2193c33bc50118d8d303
$uname <$email>
2022-11-26 17:07:54 +0000

    User ${usernamespecify} : ${descriptioncommit}

--- pushgamaiongithub.sh
+++ pushgamaiongithub.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 # Read informations on keyboard
-read -p "User Name :" username
-read -p "Password :" passwd
+username = 'ernestfotseu'
+read -p "User Name :" usernamespecify
 read -p "Your email adresse :" email
 read -p "Description of commit :" descriptioncommit
+
+acces_token = "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"
 
 # Set authentication
 git config --global user.email '${email}'
 git config --global user.name '${username}'
-git config --global user.passwd '${passwd}'
 # create a file, then add it to stage
 git init
 git add .
 echo "*****Commit description : ${descriptioncommit}*****"
-git commit -m '${descriptioncommit}'
+git commit -m 'User ${usernamespecify} : ${descriptioncommit}'
 echo "*****Remove old origin*****"
 git remote remove origin
 echo "*****Add origin*****"
-git remote add origin https://github.com/ernestfotseu/gamairoot-ce.git
+# git remote add origin https://github.com/ernestfotseu/gamairoot-ce.git
+git remote add origin https://{acces_token}@github.com/{username}/gamairoot-ce.git
 echo "*****Pull github origin*****"
 git pull origin main --rebase
 git remote -v
 echo "*****Push data*****"
-git push origin main         # push to github
+git push origin main
