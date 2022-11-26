31c3e16f9089cbde38bb8c904853fb021462e19e
$uname <$email>
2022-11-26 17:32:49 +0000

    User ernestfotseu : 1st commit

--- pushgamaiongithub.sh
+++ pushgamaiongithub.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+ACCES_TOKEN="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"
 # Read informations on keyboard
 read -p "User Name :" usernamespecify
 read -p "Your email adresse :" email
 read -p "Description of commit :" descriptioncommit
 
-acces_token = "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"
-echo "acces_token=${acces_token}"
+echo "acces_token=${ACCES_TOKEN}"
 # Set authentication
 git config --global user.email "${email}"
 git config --global user.name "${usernamespecify}"
@@ -18,8 +18,8 @@ echo "*****Remove old origin*****"
 git remote remove origin
 echo "*****Add origin*****"
 # git remote add origin https://github.com/ernestfotseu/gamairoot-ce.git
-git remote add origin https://${acces_token}@github.com/ernestfotseu/gamairoot-ce.git
-echo "https://${acces_token}@github.com/ernestfotseu/gamairoot-ce.git"
+git remote add origin https://${ACCES_TOKEN}@github.com/ernestfotseu/gamairoot-ce.git
+echo "https://${ACCES_TOKEN}@github.com/ernestfotseu/gamairoot-ce.git"
 echo "*****Pull github origin*****"
 git pull origin main --rebase
 git remote -v
